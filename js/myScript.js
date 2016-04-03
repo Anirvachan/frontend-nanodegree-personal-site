ee6460e60aca249f9c2a94ef31467f256c2df025
Anirvachan <anirv.n.b@gmail.com>
2016-04-03 07:34:39 +0530

    Update portfolio with Udacity projects.

--- js/myScript.js
+++ js/myScript.js
@@ -1,9 +1,16 @@
 var link = document.getElementsByTagName("h3");
 
-var urlList = {"Asteroids":"http://www.codeskulptor.org/#user39_wdcvKTfcrvnBY0X_6.py",
+var urlList = {"Portfolio Page":"http://anirvachan.github.io/frontend-nanodegree-personal-site",
+			   "Interactive Resume":"http://anirvachan.github.io/frontend-nanodegree-resume/",
+			   "Frogger Clone":"http://anirvachan.github.io/frontend-nanodegree-arcade-game/",
+			   "Website Optimization":"http://anirvachan.github.io/frontend-nanodegree-mobile-portfolio/",
+			   "Neighborhood Map":"http://anirvachan.github.io/frontend-nanodegree-neighborhood-map/",
+			   "Feed-reader Testing":"http://anirvachan.github.io/frontend-nanodegree-feedreader/",
+			   "Asteroids":"http://www.codeskulptor.org/#user39_wdcvKTfcrvnBY0X_6.py",
 			   "Blackjack":"http://www.codeskulptor.org/#user39_O8QIFFo8IoWewUB_14.py",
-			   "2048":"http://www.codeskulptor.org/#user40_ntIpAQogVO_4.py",
-			   "Tic-Tac-Toe":"http://www.codeskulptor.org/#user40_3WQuQVJNGS_8.py"};
+			   "2048":"http://www.codeskulptor.org/#user41_ntIpAQogVO_6.py",
+			   "Tic-Tac-Toe":"http://www.codeskulptor.org/#user40_3WQuQVJNGS_8.py",
+			   "15 Puzzle":"http://www.codeskulptor.org/#user41_vYC3Oh4dEl_0.py"};
 
 for (var index = 0; index < link.length; index++) {
 	link[index].addEventListener("mouseover", hoverFunction);
